a572c048c907d1b36572f8806cd6d9d096c004b5
Nilstrieb <48135649+Nilstrieb@users.noreply.github.com>
2022-11-20 20:29:04 +0100

    delete tokio

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,32 @@
-use futures::{stream, StreamExt};
+use std::marker::PhantomData;
+
+use futures::{stream, StreamExt, Future};
+
+pub struct JoinHandle<T> {
+    raw: Option<()>,
+    id: u64,
+    _p: PhantomData<T>,
+}
+
+impl<T> Future for JoinHandle<T> {
+    type Output = Result<T, ()>;
+
+    fn poll(self: std::pin::Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> std::task::Poll<Self::Output> {
+        loop {}
+    }
+}
+
+pub fn spawn<T>(future: T) -> JoinHandle<T::Output>
+where
+    T: Future + Send + 'static,
+    T::Output: Send + 'static,
+{
+   loop {}
+}
 
 fn main() {
-    let client = reqwest::Client::builder().build().unwrap();
-
-    // Concurrent Requests
     let bodies = stream::iter([])
-        .map(|url: String| {
-            let req = client.get(&url);
-            tokio::spawn(async move {
-                let resp = req.send().await.unwrap();
-                let text = resp.text().await.unwrap();
-                Result::Ok((url, text))
-            })
-        })
+        .map(|url: String| spawn(async move { Result::Ok((url, "A".to_owned())) }))
         .buffer_unordered(0);
 
     bodies.for_each(|b| async {
